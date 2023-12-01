09fa24c2ca408735ea256925051ac0c861bd130a
Pietromuresu <muresupietro66@gmail.com>
2023-12-01 13:19:09 +0100

    Style Hero and App and create Header file

---
+++ MovieClient/movie-api-app/src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faVideoSlash} from "@fortawesome/free-solid-svg-icons";
+
+
+
+const Header =()=>{
+  return(
+    <div>
+
+    </div>
+  )
+}
--- MovieClient/movie-api-app/src/components/hero/Hero.js
+++ MovieClient/movie-api-app/src/components/hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = ({movies}) =>{
             return(
               <Paper key={movie.imdbId}>
               <div className="movie-card-container">
-                <div className="movie-card">
+                <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
                   <div className="movie-detail"> 
                     <div className="movie-poster">
                       <img src={movie.poster}  alt={movie.title}/>
